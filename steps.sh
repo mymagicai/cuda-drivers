b08eba51fc23a0ae3d32bee1557968ac1de5e857
Vitali Avagyan <eheva87@gmail.com>
2023-09-10 18:32:57 +0100

    fix: rm redundant distribution line

--- steps.sh
+++ steps.sh
@@ -1,7 +1,6 @@
 #! /usr/bin/bash
 sudo apt-get install linux-headers-$(uname -r)
 distribution=$(. /etc/os-release;echo $ID$VERSION_ID | sed -e 's/\.//g')
-distribution=$(. /etc/os-release;echo $ID$VERSION_ID | sed -e 's/\.//g')
 sudo dpkg -i cuda-keyring_1.0-1_all.deb
 sudo apt-get update
 sudo apt-get -y install cuda-drivers

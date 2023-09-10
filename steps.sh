7cd792176c0c4d530e7c5822124c20b9b2a39a70
Vitali Avagyan <eheva87@gmail.com>
2023-09-10 18:42:23 +0100

    fix: add missing wget command

--- steps.sh
+++ steps.sh
@@ -1,6 +1,7 @@
 #! /usr/bin/bash
 sudo apt-get install linux-headers-$(uname -r)
 distribution=$(. /etc/os-release;echo $ID$VERSION_ID | sed -e 's/\.//g')
+wget https://developer.download.nvidia.com/compute/cuda/repos/$distribution/x86_64/cuda-keyring_1.0-1_all.deb
 sudo dpkg -i cuda-keyring_1.0-1_all.deb
 sudo apt-get update
 sudo apt-get -y install cuda-drivers
